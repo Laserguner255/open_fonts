f93a403dd6c264fa36bcd5fb8c119759caa5d036
Laserguner255 <138403472+Laserguner255@users.noreply.github.com>
2025-09-26 19:12:47 -0400

    Update master branch

--- README.md
+++ README.md
@@ -1,14 +1,4 @@
-# Open Fonts - [XDA Thread](https://forum.xda-developers.com/t/magisk-module-font-open-fonts.4446663/)
-
-![Open Font](https://raw.githubusercontent.com/F3FFO/Open-Fonts/master/img/logo.png)
-
 This module helps you to replace your system fonts though Magisk.
-
-**ATTENTION!** After several attempts I decided to replace only the main Android font: Roboto. This choice is the one I think is more correct as it is left to the user the possibility to choose other fonts without having to uninstall the module.
-
-### About Twemoji
-
-I extracted the font from the arch linux user repository because it is already in the [CBDT](https://learn.microsoft.com/en-us/typography/opentype/spec/cbdt)/[CBLC](https://learn.microsoft.com/en-us/typography/opentype/spec/cblc) standard.
 
 ## Compatibility
 
@@ -23,10 +13,6 @@ I extracted the font from the arch linux user repository because it is already i
 ## DISCLAIMER
 
 **I am not responsible for any damage that may occur to your device, so it is your own choice to attempt this module.**
-
-## Support
-
-If you found this helpful, please consider supporting development with a [coffe](https://www.paypal.me/f3ff0). Alternatively, you can contribute to the project by reporting bugs and doing PR. All support is appreciated!
 
 ## Features
 
@@ -59,120 +45,10 @@ If you found this helpful, please consider supporting development with a [coffe]
 
 Copyright 2022 F3FFO
 
-The source code is available under [GPL-3.0](https://github.com/Magisk-Modules-Alt-Repo/open_fonts/blob/master/LICENSE)
+The source code is available under [GPL-3.0](https://github.com/Laserguner255/open_fonts/blob/master/LICENSE)
 
 - Noto Sans/NotoSans Mono are available under the [SIL Open Font License v1.1](https://github.com/notofonts/noto-fonts/blob/main/LICENSE)
 - Roboto-Flex is available under the [SIL Open Font License v1.1](https://github.com/googlefonts/roboto-flex/blob/main/OFL.txt)
 - JetBrainsMono is available under the [SIL Open Font License v1.1](https://github.com/JetBrains/JetBrainsMono/blob/master/OFL.txt)
 - Noto Emoji is available under the [SIL Open Font License v1.1](https://github.com/googlefonts/noto-emoji/blob/main/fonts/LICENSE)
 - Twemoji is available under the [CC-BY 4.0](https://github.com/twitter/twemoji/blob/master/LICENSE-GRAPHICS)
-
-## Change logs
-
-# v3.1.1
-
-- Update link for noto sans fonts
-
-# v3.1.0
-
-- Rewrite installation script
-- Bug fix
-
-# v3.0.1
-
-- Add curl binary
-- Bug fix
-
-# v3.0.0
-
-- Added two new system fonts: OpenSans and Roboto-Flex
-- Added two new monospaced system fonts: NotoSans Mono and JetBrains Mono
-- Added a new emoji font: Twemoji
-- Decreased module size
-- Bug fix
-
-# v2.0.1
-
-- Bug fix
-
-# v2.0.0
-
-- Increased boot speed
-- Updated compatibility with Samsung devices
-- Fully compatible with Safety Net Fix
-- Update addon
-- Bug fix
-
-# v1.1.3
-
-- Optimizated installation script
-- Bug fix
-
-# v1.1.2
-
-- Decreased module size
-- Bug fix
-
-# v1.1.1
-
-- Added ability to choose whether to change the emoji font
-- Bug fix
-
-# v1.1.0
-
-- Bug fix
-
-# v1.0.9
-
-- Update to magisk 24 standard
-- Renamed Roboto to NotoSans
-- Bug fix
-
-# v1.0.8
-
-- Add support for sans monospace font
-- Add support for condensed fonts
-- Update NotoSans from source
-- Bug fix
-
-# v1.0.7
-
-- Decrease module size
-- Replaced OpenSans font with a more complete one: NotoSans
-- Add support for Thin font
-- Add support for Medium font
-- Add support for Black font
-- Changed Samsung, LG, HTC emoji with Noto Emoji
-
-# v1.0.6
-
-- Add bold font
-
-# v1.0.5
-
-- Update fonts to Android 12
-- Add fonts.xml from Android 12
-
-# v1.0.4
-
-- Changed monospace font
-
-# v1.0.3
-
-- Update to last version some Noto fonts
-- Changed Rubik font
-- Changed Lato font
-
-# v1.0.2
-
-- Change SourceSansPro fonts
-
-# v1.0.1
-
-- Change secondary system fonts
-- Create AndroidClock font from Open Sans
-- Add NotoColorEmoji (unicode 14)
-
-# v1.0.0
-
-- Initial release
--- common/install.sh
+++ common/install.sh
@@ -5,38 +5,16 @@ STATUS=0
 SKIP_INSTALLATION=0
 NEXT_SELECTION=1
 
-noto_sans() {
-	FONT_NAME="NotoSans"
-	FONT_URL=https://github.com/notofonts/notofonts.github.io/raw/main/fonts/NotoSans/full/variable-ttf/NotoSans%5Bwdth%2Cwght%5D.ttf
-	FONT_NAME_ITALIC="NotoSans-Italic"
-	FONT_URL_ITALIC=https://github.com/notofonts/notofonts.github.io/raw/main/fonts/NotoSans/full/variable-ttf/NotoSans-Italic%5Bwdth%2Cwght%5D.ttf
+system_font() {
+	FONT_NAME="FONT_NAME"
+	FONT_URL=
+	FONT_NAME_ITALIC="FONT_NAME-Italic"
+	FONT_URL_ITALIC=
 }
-open_sans() {
-	FONT_NAME="OpenSans"
-	FONT_URL=https://github.com/googlefonts/opensans/raw/main/fonts/noto-set/variable/OpenSans%5Bwdth%2Cwght%5D.ttf
-	FONT_NAME_ITALIC="OpenSans-Italic"
-	FONT_URL_ITALIC=https://github.com/googlefonts/opensans/raw/main/fonts/noto-set/variable/OpenSans-Italic%5Bwdth%2Cwght%5D.ttf
-}
-robot_flex() {
-	FONT_NAME="Roboto-Flex"
-	FONT_URL=https://github.com/googlefonts/roboto-flex/raw/main/fonts/RobotoFlex%5BGRAD%2CXOPQ%2CXTRA%2CYOPQ%2CYTAS%2CYTDE%2CYTFI%2CYTLC%2CYTUC%2Copsz%2Cslnt%2Cwdth%2Cwght%5D.ttf
-	FONT_NAME_ITALIC=""
-}
-noto_sans_mono() {
-	FONT_NAME="NotoSansMono"
-	FONT_URL=https://github.com/notofonts/notofonts.github.io/raw/main/fonts/NotoSansMono/unhinted/variable-ttf/NotoSansMono%5Bwdth%2Cwght%5D.ttf
-}
-jetbrains_mono() {
-	FONT_NAME="JetBrainsMono"
-	FONT_URL=https://github.com/JetBrains/JetBrainsMono/raw/master/fonts/variable/JetBrainsMono%5Bwght%5D.ttf
-}
-noto_color_emoji() {
-	FONT_NAME="NotoColorEmoji"
-	FONT_URL=https://github.com/googlefonts/noto-emoji/raw/main/fonts/NotoColorEmoji.ttf
-}
-twemoji() {
-	FONT_NAME="Twemoji"
-	FONT_URL=https://github.com/Magisk-Modules-Alt-Repo/open_fonts/raw/develop/sources/twemoji/Twemoji.ttf
+
+monospace_font() {
+	FONT_NAME="FONT_NAME-Mono"
+	FONT_URL=
 }
 download_file() {
 	STATUS = 0
@@ -110,28 +88,10 @@ install_mono_font() {
 	ln -s ./"$STANDARD_FONT_NAME" $STANDARD_FOLDER/CutiveMono.ttf
 	ui_print "  Fonts replaced!"
 }
-install_emoji_font() {
-	STANDARD_FONT_NAME="NotoColorEmoji.ttf"
-	ui_print "    Replacing..."
-	cp_ch "$TMPDIR"/"${FONT_NAME}.ttf" $STANDARD_FOLDER/"$STANDARD_FONT_NAME"
-
-	if [ $API -ge 31 ]; then
-		ln -s ./"$STANDARD_FONT_NAME" $STANDARD_FOLDER/NotoColorEmojiFlags.ttf
-	fi
-
-	if [ $MANUFACTER = "Samsung" ]; then
-		ln -s ./"$STANDARD_FONT_NAME" $STANDARD_FOLDER/SamsungColorEmoji.ttf
-	elif [ $MANUFACTER = "LGE" ]; then
-		ln -s ./"$STANDARD_FONT_NAME" $STANDARD_FOLDER/LGNotoColorEmoji.ttf
-	elif [ $MANUFACTER = "HTC" ]; then
-		ln -s ./"$STANDARD_FONT_NAME" $STANDARD_FOLDER/HTC_ColorEmoji.ttf
-	fi
-	ui_print "  Fonts replaced!"
-}
 check_status() {
 	if [ "${STATUS}" -eq 0 ]; then
 		ui_print ""
-		ui_print "  !!! Dowload failed !!!"
+		ui_print "  !!! Download failed !!!"
 		ui_print ""
 		exit 1
 	fi
@@ -144,130 +104,32 @@ clean_up() {
 	rm -rf $STANDARD_FOLDER/placeholder
 }
 
-ui_print "- Do you want to replace system font?"
+# replace system font
+ui_print "- Do you want to replace the system font?"
 ui_print "  [Vol+ = yes, Vol- = no]"
 if chooseport 30; then
-	ui_print "    Choose between:"
-	ui_print "      NotoSans, OpenSans, Roboto-Flex"
-	sleep 3
-	ui_print ""
-	ui_print "    Select:"
-	ui_print "    -> NotoSans [Vol+ = yes, Vol- = no]"
-	if chooseport 3; then
-		noto_sans
-		NEXT_SELECTION=0
-	fi
-	if [ "${NEXT_SELECTION}" -eq 1 ]; then
-		ui_print "    -> OpenSans [Vol+ = yes, Vol- = no]"
-		if chooseport 3; then
-			open_sans
-			NEXT_SELECTION=0
-		fi
-	fi
-	if [ "${NEXT_SELECTION}" -eq 1 ]; then
-		ui_print "    -> Roboto-Flex [Vol+ = yes, Vol- = no]"
-		if chooseport 3; then
-			robot_flex
-		else
-			SKIP_INSTALLATION=1
-		fi
-	fi
-
-	if [ "${SKIP_INSTALLATION}" -eq 0 ]; then
-		download_file $FONT_NAME $FONT_URL
-		if [ $API -lt 31 ]; then
-			if [[ ! -z "${FONT_NAME_ITALIC}" ]]; then
-				download_file $FONT_NAME_ITALIC $FONT_URL_ITALIC
-			fi
-		fi
-
-		check_status
-		install_font
-
-		ui_print ""
-		ui_print " !! Experimental feature, may not works !!"
-		ui_print "    Do you want to replace android clock font?"
-		ui_print "    [Vol+ = yes, Vol- = no]"
-		if chooseport 3; then
-			install_clock_font
-		else
-    		ui_print "  Android clock font not replaced."
-  		fi
-		ui_print "  Fonts replaced!"
-	else
-		ui_print "  Fonts will not be replaced!"
-	fi
+	system_font
+	download_file $FONT_NAME $FONT_URL
+	download_file $FONT_NAME_ITALIC $FONT_URL_ITALIC
+	check_status
+	install_font
+	install_clock_font
+	ui_print "  System font replaced!"
+else
+	ui_print "  System font not replaced"
 fi
 
 # replace monospaced font
 reset_variable
-ui_print "- Do you want to replace monospaced font?"
+ui_print "- Do you want to replace the monospace font?"
 ui_print "  [Vol+ = yes, Vol- = no]"
 if chooseport 30; then
-	ui_print "    Choose between:"
-	ui_print "      NotoSans Mono, JetBrains Mono"
-	sleep 3
-	ui_print ""
-	ui_print "    Select:"
-	ui_print "    -> NotoSans Mono [Vol+ = yes, Vol- = no]"
-	if chooseport 3; then
-		noto_sans_mono
-		NEXT_SELECTION=0
-	fi
-	if [ "${NEXT_SELECTION}" -eq 1 ]; then
-		ui_print "    -> JetBrainsMono [Vol+ = yes, Vol- = no]"
-		if chooseport 3; then
-			jetbrains_mono
-		else
-			SKIP_INSTALLATION=1
-		fi
-	fi
-
-	if [ "${SKIP_INSTALLATION}" -eq 0 ]; then
-		download_file $FONT_NAME $FONT_URL
-		check_status
-
-		install_mono_font
-	else
-		ui_print "  Fonts will not be replaced!"
-	fi
+	monospace_font
+	download_file $FONT_NAME $FONT_URL
+	check_status
+	install_mono_font
+	ui_print "  Monospace font Replaced"
 else
-	ui_print "  Fonts will not be replaced!"
-fi
-
-# replace emoji
-reset_variable
-ui_print "- Do you want to replace system emoji?"
-ui_print "  [Vol+ = yes, Vol- = no]"
-if chooseport 30; then
-	ui_print "    Choose between:"
-	ui_print "      NotoColorEmoji, Twemoji"
-	sleep 3
-	ui_print ""
-	ui_print "    Select:"
-	ui_print "    -> NotoColorEmoji [Vol+ = yes, Vol- = no]"
-	if chooseport 3; then
-		noto_color_emoji
-		NEXT_SELECTION=0
-	fi
-	if [ "${NEXT_SELECTION}" -eq 1 ]; then
-		ui_print "    -> Twemoji [Vol+ = yes, Vol- = no]"
-		if chooseport 3; then
-			twemoji
-		else
-			SKIP_INSTALLATION=1
-		fi
-	fi
-
-	if [ "${SKIP_INSTALLATION}" -eq 0 ]; then
-		download_file $FONT_NAME $FONT_URL
-		check_status
-
-		install_emoji_font
-	else
-		ui_print "  Fonts will not be replaced!"
-	fi
-else
-	ui_print "  Fonts will not be replaced!"
+	ui_print "  Monospace font not replaced"
 fi
 clean_up
